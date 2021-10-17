d06c8fad82842fda01a0d53199a1feed17d329e1
celsoernani <celsoernane@gmail.com>
2021-10-17 10:55:01 -0300

    doc

--- src/routes/bancoInterno.routes.ts
+++ src/routes/bancoInterno.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { BancoInternoController } from "../resources/BancoInterno/controllers/BancoInternoController.controller";
 
+import { BancoInternoController } from "../resources/BancoInterno/controllers/BancoInternoController.controller";
 
 const bancoInternoRoutes = Router();
 const bancoInternoController = new BancoInternoController();
 
-bancoInternoRoutes.get("/banco-interno/mocked", bancoInternoController.getMockedInfo)
+bancoInternoRoutes.get(
+  "/banco-interno/mocked",
+  bancoInternoController.getMockedInfo
+);
 
 export default bancoInternoRoutes;
-
